0422fa2254b71982814e1738fbdbfb0fa7cf019c
Iudin_Aleksei <aviordream@gmail.com>
2020-12-26 15:32:00 +0300

    feat: add debounce

--- src/js/controllers/filter.js
+++ src/js/controllers/filter.js
@@ -2,9 +2,18 @@ import { CLASS_LIST } from '../common/constants';
 
 const inputFilterHandler = (renderTable, filterPersons, persons) => {
   const input = document.querySelector(`.${CLASS_LIST.filter}`);
+  let isRunnable = true;
   input.addEventListener('input', (event) => {
-    const pattern = event.target.value;
-    renderTable(filterPersons(pattern, persons));
+    if (!isRunnable) {
+      return;
+    }
+
+    isRunnable = false;
+
+    setTimeout(() => {
+      isRunnable = true;
+      renderTable(filterPersons(event.target.value, persons));
+    }, 500);
   });
 };
 
